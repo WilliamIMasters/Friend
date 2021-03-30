ba7e594217a7957c23a1516519d6469dfb5ef7d0
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2021-03-30 23:53:06 +0100

    Added bow script and image

---
+++ Friend/Assets/BowScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour
+{
+    public GameObject bow;
+
+    public float rotationSpeed = 10f;
+    public float radius = 2f;
+    public float radiusSpeed = 2f;
+
+    private Vector3 desiredPos;
+
+    void Start()
+    {
+        bow.transform.position = (bow.transform.position - transform.position).normalized * radius + transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Rotate around player
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y - playerScreenPos.y, mousePos.x - playerScreenPos.x) * Mathf.Rad2Deg;
+        float currentAngle = bow.transform.eulerAngles.z;
+
+        bow.transform.RotateAround(transform.position, new Vector3(0, 0, 1), angle - currentAngle);
+
+
+
+        // Fire
+        if(Input.GetMouseButtonDown(0))
+        {
+            print("fire");
+        }
+
+
+    }
+}
