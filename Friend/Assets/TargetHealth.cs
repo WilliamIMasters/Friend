8a3e85c6f68e5989da237a58cbc343cc607f8aca
wmastersPlym <31853465+sidskater@users.noreply.github.com>
2021-03-31 16:50:16 +0100

    added target health

---
+++ Friend/Assets/TargetHealth.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHealth : MonoBehaviour
+{
+
+    public int healthPoints;
+
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            flashRed();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Projectile" && collision.gameObject.GetComponent<projectile>())
+        {
+            this.takeDamage(collision.gameObject.GetComponent<projectile>().damage);
+            collision.gameObject.GetComponent<projectile>().freeze();
+            collision.gameObject.transform.parent = gameObject.transform;
+            //collision.gameObject.GetComponent<projectile>().destroy();
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        healthPoints -= damage;
+        flashRed();
+        if(healthPoints <= 0)
+        {
+            die();
+        }
+    }
+
+    public void die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void flashRed()
+    {
+        Color original = sr.color;
+        sr.color = Color.red;
+        StartCoroutine(flashOriginal(original));
+    }
+
+    private IEnumerator flashOriginal(Color original)
+    {
+        yield return new WaitForSeconds(0.1f);
+        sr.color = original;
+    }
+}
--- Friend/Assets/scripts/projectile.cs
+++ Friend/Assets/scripts/projectile.cs
@@ -8,19 +8,43 @@ public class projectile : MonoBehaviour
     public float ttl;
     public float speed;
 
+    public int damage;
+
     private Rigidbody2D rb;
+
+    private bool toDestroy;
 
     // Start is called before the first frame update
     void Start()
     {
+        toDestroy = true;
         rb = GetComponent<Rigidbody2D>();
         rb.AddRelativeForce(transform.right * speed * 100);
         //rb.velocity += new Vector2(transform.forward.x * speed, transform.forward.y * speed);
+
+        StartCoroutine(kill());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator kill()
     {
-        
+        yield return new WaitForSeconds(ttl);
+
+        if(toDestroy)
+        {
+            destroy();
+        }
+    }
+
+    public void destroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void freeze()
+    {
+        toDestroy = false;
+        rb.velocity = Vector3.zero;
+        rb.bodyType = RigidbodyType2D.Kinematic;
     }
 }
